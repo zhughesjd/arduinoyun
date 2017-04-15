4d534a49f745d7a6594536659d7ed903168455fa
joshua hughes <z_hughesjd@yahoo.com>
2017-04-15 11:51:51 -0400

    adding more sensors

---
+++ Joystick/Joystick.ino
@@ -0,0 +1,22 @@
+#include <Console.h>
+int sensorPin = 5;
+int value = 0;
+void setup() {
+  Bridge.begin();
+  Console.begin();
+  while (!Console);
+  pinMode(7, OUTPUT);
+}
+void loop() {
+  value = analogRead(0);
+  Console.print("X:");
+  Console.print(value, DEC);
+  value = analogRead(1);
+  Console.print(" | Y:");
+  Console.print(value, DEC);
+  value = digitalRead(7);
+  Console.print(" | Z: ");
+  Console.println(value, DEC);
+  delay(100);
+}
+
---
+++ Relay/Relay.ino
@@ -0,0 +1,26 @@
+/* use Relay module and HC06 bluetooth receiver to turn on/off LEd wirlessly
+ */
+#include <Console.h>
+
+int relay = 10; 
+void setup()
+{
+    Bridge.begin();
+    Console.begin();
+    while(!Console);
+   pinMode(relay,OUTPUT); 
+ }
+void loop()
+{
+   char val,old_val; 
+  val = Console.read(); 
+  old_val=val;
+  if (old_val=='1' ) 
+ {
+   digitalWrite(relay,HIGH); 
+  }
+  if(old_val=='0')  
+  {
+    digitalWrite(relay,LOW); 
+  }
+ }
